75dfdbf5c1e684be0dd28ee1c2e74d6300a2c975
CC Sun <toletgo101214@gmail.com>
2025-05-09 15:49:46 +0800

    add profile route

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Routes, Route } from 'react-router-dom';
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "./assets/styles/user-all.scss"
+
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup'
+import EditProfile from './pages/EditProfile/EditProfile';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/member/profile" element={<EditProfile />}></Route>
       </Routes>
     </>
   );
