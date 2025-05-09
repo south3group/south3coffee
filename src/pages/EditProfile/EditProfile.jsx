e39aed2273bc42c784cc19bda4fdad548989f741
CC Sun <toletgo101214@gmail.com>
2025-05-09 15:18:33 +0800

    add edit-profile

---
+++ src/pages/EditProfile/EditProfile.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import DatePicker from "react-datepicker";
+
+import axios from "axios";
+
+import MemberSidebar from "../../components/MemberSidebar/MemberSidebar";
+import Footer from "../../components/Footer/Footer";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+function EditProfile() {
+    <div>
+        <Footer />
+    </div>
+}
+
+export default EditProfile;
