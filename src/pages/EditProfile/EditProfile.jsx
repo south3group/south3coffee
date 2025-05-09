451ea4b55610231e45a8b21b305211db8e7d4841
CC Sun <toletgo101214@gmail.com>
2025-05-09 15:27:34 +0800

    add state and handlers for profile form validation and modal control

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,18 +1,66 @@
-import { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
-import DatePicker from "react-datepicker";
+import { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
 
-import axios from "axios";
+import axios from 'axios';
 
-import MemberSidebar from "../../components/MemberSidebar/MemberSidebar";
-import Footer from "../../components/Footer/Footer";
+import MemberSidebar from '../../components/MemberSidebar/MemberSidebar';
+import Footer from '../../components/Footer/Footer';
 
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 
 function EditProfile() {
-    <div>
-        <Footer />
+    const [userData, setUserData] = useState({
+        userName: "",
+        userGender: "",
+        userBirth: null,
+        userPhone: "",
+        userAddress: "",
+      });
+    
+      const [formValidated, setFormValidated] = useState(false);
+      const [fieldErr, setFieldErr] = useState({});
+    
+      const [isOpen, setIsOpen] = useState(false);
+      const [modalTitle, setModalTitle] = useState("");
+      const [modalMsg, setModalMsg] = useState("");
+      const [modalType, setModalType] = useState("");
+    
+      const datepickerRef = useRef(null);
+      const navigate = useNavigate();
+    
+      const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+        setFieldErr((prevErr) => ({
+          ...prevErr,
+          [name]: "",
+        }));
+      };
+    
+      const handleCloseModal = () => {
+        setIsOpen(false);
+        if (modalType === "toLogin") {
+          navigate("/login");
+        }
+      };
+    
+      const apiUrl = import.meta.env.VITE_API_URL;
+      const route = `${apiUrl}/api/v1/users/membership/profile`;
+
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      <div className="flex-grow-1">
+        <MemberSidebar>
+
+        </MemberSidebar>
+      </div>
+      <Footer />
     </div>
+  );
 }
 
 export default EditProfile;
