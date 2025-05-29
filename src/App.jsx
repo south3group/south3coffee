a79ade707dd4281a3edc87bc624cde748cf95bc9
CC Sun <toletgo101214@gmail.com>
2025-05-30 00:51:51 +0800

    feat: Add GenRyuMin2 font family with multiple weights

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import './assets/styles/font-classes.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import "./assets/styles/user-all.scss"
+import './assets/styles/user-all.scss';
 
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import HomePage from './pages/HomePage/HomePage';
 import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup'
+import Signup from './pages/Signup/Signup';
 import EditProfile from './pages/EditProfile/EditProfile';
 import Contact from './pages/Contact/Contact';
 import Admin from './pages/Admin/Admin';
 
-import "./assets/styles/user-datepicker.scss"
+import './assets/styles/user-datepicker.scss';
 
-const App = ()=>{
+const App = () => {
   return (
     <>
       <ScrollToTop />
       <Routes>
-      <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/member/profile" element={<EditProfile />}></Route>
@@ -28,6 +29,6 @@ const App = ()=>{
       </Routes>
     </>
   );
-}
+};
 
 export default App;
