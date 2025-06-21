5c097c114a4a2e0682025ab0589d29f1c5b1d655
CC Sun <toletgo101214@gmail.com>
2025-06-22 04:17:22 +0800

    Connect Create Order page to GET order review API with auth check

--- src/pages/CreateOrder/CreateOrder.jsx
+++ src/pages/CreateOrder/CreateOrder.jsx
@@ -1,10 +1,51 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import axios from 'axios';
+
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import { images } from '../../constants/image';
 
-// 需要做強制登入驗證
-
 const CreateOrder = () => {
+  const [orderData, setOrderData] = useState(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalMsg, setModalMsg] = useState('');
+
+  const navigate = useNavigate();
+
+  // 取得訂單
+  useEffect(() => {
+    const fetchOrderReview = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        const res = await axios.get(
+          'http://localhost:8080/api/v1/users/orderReview',
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          },
+        );
+        setOrderData(res.data.data);
+      } catch (err) {
+        if (err.response?.status === 401) {
+          localStorage.removeItem('token');
+          navigate('/');
+        } else {
+            const msg = err.response?.data?.message || '發生錯誤';
+            setModalMsg(msg);
+            setIsOpen(true);
+        }
+      }
+    };
+
+    fetchOrderReview();
+  }, [navigate]);
+
+  if (!orderData) return <div>Loading...</div>;
+
   return (
     <>
       <Header />
@@ -31,33 +72,34 @@ const CreateOrder = () => {
                   {/* 訂單詳情 */}
                   {/* 手機版 */}
                   <div className="d-block d-md-none orders-custom-mobile">
-                    <div className="order-card">
-                      <h4 className="check-title m-0">確認訂單內容</h4>
-                      <div className="order-card-content">
-                        <div className="text-group">
-                          <p className="m-0 text-group-title">購買品項</p>
-                          <p className="m-0 text-group-text">
-                            哥倫比亞咖啡豆(300g)
-                          </p>
-                        </div>
-                        <div className="text-group">
-                          <p className="m-0 text-group-title">單價</p>
-                          <p className="m-0 text-group-text">
-                            NTD$ <span className="text-group-text">600</span>
-                          </p>
-                        </div>
-                        <div className="text-group">
-                          <p className="m-0 text-group-title">數量</p>
-                          <p className="m-0 text-group-text">3</p>
-                        </div>
-                        <div className="text-group">
-                          <p className="m-0 text-group-title">總金額</p>
-                          <p className="m-0 text-group-text">
-                            NTD$ <span className="text-group-text">1,800</span>
-                          </p>
+                    {orderData.orderItems.map((item, idx) => (
+                      <div className="order-card" key={idx}>
+                        <div className="order-card-content">
+                          <div className="text-group">
+                            <p className="text-group-title m-0">購買品項</p>
+                            <p className="text-group-text m-0">{item.name}</p>
+                          </div>
+                          <div className="text-group">
+                            <p className="text-group-title m-0">單價</p>
+                            <p className="text-group-text m-0">
+                              NTD$ {item.price}
+                            </p>
+                          </div>
+                          <div className="text-group">
+                            <p className="text-group-title m-0">數量</p>
+                            <p className="text-group-text m-0">
+                              {item.quantity}
+                            </p>
+                          </div>
+                          <div className="text-group">
+                            <p className="text-group-title m-0">總金額</p>
+                            <p className="text-group-text m-0">
+                              NTD$ {item.subtotal}
+                            </p>
+                          </div>
                         </div>
                       </div>
-                    </div>
+                    ))}
 
                     {/* 桌電版 */}
                     <table className="d-none d-md-table table orders-custom-table m-0 ">
@@ -143,48 +185,24 @@ const CreateOrder = () => {
                       </tr>
                     </thead>
                     <tbody className="orders-custom-tbody">
-                      <tr>
-                        <th scope="row" className="orders-custom-tbody-th">
-                          嘉義阿里山咖啡豆(500g)
-                        </th>
-                        <td className="orders-custom-tbody-td price-box">
-                          <p className="box-currency m-0">NTD$</p>
-                          <p className="box-price m-0">500</p>
-                        </td>
-                        <td className="orders-custom-tbody-td">2</td>
-                        <td className="orders-custom-tbody-td price-box">
-                          <p className="box-currency m-0">NTD$</p>
-                          <p className="box-price m-0">1,000</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th scope="row" className="orders-custom-tbody-th">
-                          哥倫比亞咖啡豆(300g)
-                        </th>
-                        <td className="orders-custom-tbody-td price-box">
-                          <p className="box-currency m-0">NTD$</p>
-                          <p className="box-price m-0">600</p>
-                        </td>
-                        <td className="orders-custom-tbody-td">3</td>
-                        <td className="orders-custom-tbody-td price-box">
-                          <p className="box-currency m-0">NTD$</p>
-                          <p className="box-price m-0">1,800</p>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th scope="row" className="orders-custom-tbody-th">
-                          雛型咖啡濾紙100張
-                        </th>
-                        <td className="orders-custom-tbody-td price-box">
-                          <p className="box-currency m-0">NTD$</p>
-                          <p className="box-price m-0">150</p>
-                        </td>
-                        <td className="orders-custom-tbody-td">5</td>
-                        <td className="orders-custom-tbody-td price-box">
-                          <p className="box-currency m-0">NTD$</p>
-                          <p className="box-price m-0">750</p>
-                        </td>
-                      </tr>
+                      {orderData.orderItems.map((item, idx) => (
+                        <tr key={idx}>
+                          <td className="orders-custom-tbody-td">
+                            {item.name}
+                          </td>
+                          <td className="orders-custom-tbody-td price-box">
+                            <p className="box-currency m-0">NTD$</p>
+                            <p className="box-price m-0">{item.price}</p>
+                          </td>
+                          <td className="orders-custom-tbody-td">
+                            {item.quantity}
+                          </td>
+                          <td className="orders-custom-tbody-td price-box">
+                            <p className="box-currency m-0">NTD$</p>
+                            <p className="box-price m-0">{item.subtotal}</p>
+                          </td>
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
                 </div>
@@ -196,13 +214,13 @@ const CreateOrder = () => {
                       <div className="check-receiver-content">
                         <p className="check-receiver-content-title">收件人</p>
                         <p className="check-receiver-content-text">
-                          喃喃喃喃喃
+                          {orderData.receiver.name}
                         </p>
                       </div>
                       <div className="check-receiver-content">
                         <p className="check-receiver-content-title">連絡電話</p>
                         <p className="check-receiver-content-text">
-                          0912345678
+                          {orderData.receiver.phone}
                         </p>
                       </div>
                       <div className="check-receiver-content">
@@ -212,7 +230,7 @@ const CreateOrder = () => {
                       <div className="check-receiver-content">
                         <p className="check-receiver-content-title">詳細地址</p>
                         <p className="check-receiver-content-text">
-                          高雄市前金區中正四路211號25樓地址超常會以這種方式換行讓我看一下怎麼換
+                          {orderData.receiver.address}
                         </p>
                       </div>
                     </div>
@@ -225,28 +243,36 @@ const CreateOrder = () => {
                         <p className="product-title m-0">商品金額</p>
                         <div className="price-box">
                           <p className="product-price m-0">NTD$</p>
-                          <p className="product-price m-0">3,550</p>
+                          <p className="product-price m-0">
+                            {orderData.cost_summary.totalPrice}
+                          </p>
                         </div>
                       </div>
                       <div className="price-detail">
                         <p className="account-title m-0">運費</p>
                         <div className="price-box">
                           <p className="account-price m-0">NTD$</p>
-                          <p className="account-price m-0">0</p>
+                          <p className="account-price m-0">
+                            {orderData.cost_summary.shipping_fee}
+                          </p>
                         </div>
                       </div>
                       <div className="price-detail">
                         <p className="account-title m-0">小計</p>
                         <div className="price-box">
                           <p className="account-price m-0">NTD$</p>
-                          <p className="account-price m-0">0</p>
+                          <p className="account-price m-0">
+                            {orderData.cost_summary.grand_total}
+                          </p>
                         </div>
                       </div>
                       <div className="price-detail">
                         <p className="account-title m-0">折扣金額</p>
                         <div className="price-box">
                           <p className="account-price m-0">NTD$</p>
-                          <p className="account-price m-0">0</p>
+                          <p className="account-price m-0">
+                            {orderData.cost_summary.discount_amount}
+                          </p>
                         </div>
                       </div>
 
@@ -256,7 +282,9 @@ const CreateOrder = () => {
                         <p className="total-title m-0">金額總計</p>
                         <div className="price-box">
                           <p className="total-price m-0">NTD$</p>
-                          <p className="total-price m-0">3,550</p>
+                          <p className="total-price m-0">
+                            {orderData.cost_summary.grand_total}
+                          </p>
                         </div>
                       </div>
 
@@ -280,6 +308,41 @@ const CreateOrder = () => {
         </div>
         <div></div>
       </div>
+
+      {/* Modal */}
+      {isOpen && (
+        <div
+          className="modal show fade d-block custom-modal"
+          tabIndex="-1"
+          role="dialog"
+        >
+          <div className="modal-dialog custom-modal-dialog">
+            <div className="modal-content custom-modal-content">
+              <div className="modal-header custom-modal-header">
+                <h5 className="custom-modal-title">登入失敗</h5>
+                <button
+                  type="button"
+                  className="custom-modal-close"
+                  onClick={() => setIsOpen(false)}
+                >
+                  ✕
+                </button>
+              </div>
+              <div className="modal-body custom-modal-body">{modalMsg}</div>
+              <div className="modal-footer custom-modal-footer">
+                <button
+                  type="button"
+                  className="custom-modal-btn"
+                  onClick={() => setIsOpen(false)}
+                >
+                  關閉
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
       <Footer />
     </>
   );
